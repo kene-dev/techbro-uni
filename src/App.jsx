12089ad3db4d27449a601839b3262ceb055b993b
Gerald Ndulue <gerryndulue@gmail.com>
2025-07-08 20:42:10 +0100

    lenis added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import { Route, Routes } from "react-router"
 import Homepage from "./pages/Homepage"
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 function App() {
-
+useEffect(() => {
+    const lenis = new Lenis();
+    // Use requestAnimationFrame to continuously update the scroll
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+    },[])
 
   return (
     <Routes>
